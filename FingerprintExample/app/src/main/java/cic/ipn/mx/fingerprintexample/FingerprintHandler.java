1125a93c5f4eaeefb368f1610beebbe1960f1e6a
Manuel Pérez <jmperezsantos@gmail.com>
2018-08-18 18:37:35 -0500

    FingerprintExample

---
+++ FingerprintExample/app/src/main/java/cic/ipn/mx/fingerprintexample/FingerprintHandler.java
@@ -0,0 +1,83 @@
+package cic.ipn.mx.fingerprintexample;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.os.CancellationSignal;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+@TargetApi(Build.VERSION_CODES.M)
+public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
+
+    private CancellationSignal cancellationSignal;
+    private Context appContext;
+
+    private IFingerprintHandlerListener listener;
+
+    public FingerprintHandler(Context context, IFingerprintHandlerListener listener) {
+        appContext = context;
+        this.listener = listener;
+    }
+
+    public void startAuth(FingerprintManager manager,
+                          FingerprintManager.CryptoObject cryptoObject) {
+
+        cancellationSignal = new CancellationSignal();
+
+        if (ActivityCompat.checkSelfPermission(appContext,
+                Manifest.permission.USE_FINGERPRINT) !=
+                PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
+
+    @Override
+    public void onAuthenticationError(int errMsgId,
+                                      CharSequence errString) {
+        Toast.makeText(appContext,
+                "Authentication error\n" + errString,
+                Toast.LENGTH_LONG).show();
+
+        listener.onResult(false);
+    }
+
+    @Override
+    public void onAuthenticationHelp(int helpMsgId,
+                                     CharSequence helpString) {
+        Toast.makeText(appContext,
+                "Authentication help\n" + helpString,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onAuthenticationFailed() {
+        Toast.makeText(appContext,
+                "Authentication failed.",
+                Toast.LENGTH_LONG).show();
+
+        listener.onResult(false);
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(
+            FingerprintManager.AuthenticationResult result) {
+
+        Toast.makeText(appContext,
+                "Authentication succeeded.",
+                Toast.LENGTH_LONG).show();
+
+        listener.onResult(true);
+    }
+
+    public interface IFingerprintHandlerListener{
+
+        void onResult(boolean success);
+
+    }
+}
