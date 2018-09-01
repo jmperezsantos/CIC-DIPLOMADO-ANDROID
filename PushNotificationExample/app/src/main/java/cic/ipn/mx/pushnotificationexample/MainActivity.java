e5843e3b4c1fd23afd43ace0bf2789de1cefa8e2
Manuel Pérez <jmperezsantos@gmail.com>
2018-08-31 20:41:53 -0500

    PushNotificationExample

---
+++ PushNotificationExample/app/src/main/java/cic/ipn/mx/pushnotificationexample/MainActivity.java
@@ -0,0 +1,13 @@
+package cic.ipn.mx.pushnotificationexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
